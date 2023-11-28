ce484496c19f3ea6e271f35931a46b42d62135c7
jvilinsky <145070574+jvilinsky@users.noreply.github.com>
2023-11-28 01:45:08 -0500

    Add files via upload

--- AirbreathingProject2/Parameters.py
+++ AirbreathingProject2/Parameters.py
@@ -10,12 +10,6 @@ polyE_c = 0.9
 polyE_f = 0.92
 v2_v1_max = 0.65
 
-##For Turbine##
-To4 = 2275 #K
-polyE_t = 0.92
-phi_min = 0.78 
-psi_max = 3.3
-
 Ta = 288 #K
 g_a = 1.4
 g_g = 1.33
@@ -28,3 +22,11 @@ n1_nf = (1/polyE_f)*((g_a-1)/(g_a))
 n1_nc = (1/polyE_c)*((g_a-1)/(g_a))
 lamb2 = 0.98
 
+
+##For Turbine##
+To4 = 2275 #K
+polyE_t = 0.92
+phi_min = 0.78 
+psi_max = 3.3
+
+lamb_n = 0.05
--- AirbreathingProject2/turbine.py
+++ AirbreathingProject2/turbine.py
@@ -5,11 +5,23 @@ class Turbine:
     def __init__(self):
         self.Ut = 350
         self.Ca = 200
-        self.N = 180.61 
-        self.To2_To1 = 435.61
 
-    def turbine(self):
-        Wc = cpa*self.To2_To1
-        dTos = Wc/cpg
-        psi = (2*cpg*dTos)/self.Ut**2
+    def turb(self, T02_T01, N, T0_comb, T0_fan):
+        phi = self.Ca/self.Ut  #Flow coefficient
+        f_ideal = ((cpg*(1/1000) * To4) - (cpa*(1/1000) * T0_comb)) / ((43100) - (cpg*(1/1000) * To4))
+        mc = ma / (BPR+1)
+        mf = mc * f_ideal 
+        mt = mf + mc
+
+        dT_comp = T0_comb - T0_fan
+        w_tot = (mc * cpa*(1/1000) * dT_comp) + (ma * cpa*(1/1000) * T02_T01)
+        delta_T_turb = w_tot / (mt * cpg*(1/1000))
+        
+
+T0_fan = 319.723
+T0_comb = 755.33
+T02_T01 = 435.61 
+N = 180.61
+turbine = Turbine()
+turbine.turb(T02_T01,N,T0_comb,T0_fan)
         
